9e351a23ec62c94ed666a380da194e0c1384a7a9
Steven Deferme <sdeferme@quant.be>
2020-03-07 16:20:05 +0100

    rendering user list in sidebar S17E172

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,7 @@ const $locationButton = document.getElementById("loc");
 // Templates
 const $messageTemplate = document.getElementById('message-template').innerHTML
 const $locationMessageTemplate = document.getElementById('location-message-template').innerHTML
+const $sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -35,6 +36,13 @@ socket.on('locationMessage', (message) => {
 	$messages.insertAdjacentHTML('beforeend', html)
 })
 
+socket.on('roomData', ({ room, users }) => {
+	const html = Mustache.render($sidebarTemplate, {
+		room, 
+		users
+	})
+	document.querySelector('#sidebar').innerHTML = html
+})
 
 $messageForm.addEventListener('submit', (e) => {
 	e.preventDefault()
